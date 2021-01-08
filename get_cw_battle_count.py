facccc5850246327654d5aef5c4e29c83df00d0b
Jason Tan <1337flite@gmail.com>
2021-01-08 15:07:56 +1030

    A script to pull the number of battles a clan's members have played in a clan wars season/event.

--- get_cw_battle_count.py
+++ get_cw_battle_count.py
@@ -7,7 +7,7 @@
 import requests
 import sys
 import json
-
+import math
 
 
 #clan_id=sys.argv[1]
@@ -21,7 +21,11 @@ import json
 application_id = sys.argv[1]
 clan_id = sys.argv[2]
 season_id = sys.argv[3]
+gold = None
 
+if 5 == len(sys.argv):
+  gold = int(sys.argv[4])
+  print "got gold: " + str(gold)
 
 region_api_server="https://api.worldoftanks.asia"
 # Asia https://api.worldoftanks.asia
@@ -41,6 +45,8 @@ clan_total_battles = 0
 member_list = d['data'][clan_id]['members']
 member_list_len = len(member_list)
 
+shares = dict()
+
 print "player,account_id,battles"
 for m in member_list:
   member_name = m['account_name']
@@ -54,6 +60,29 @@ for m in member_list:
   if "None"  == str(battle_count):
     battle_count=0
   print member_name + "," + str(account_id) + "," + str(battle_count)
+  shares[str(account_id)]={'account_name':member_name,'battles':battle_count,'share':None}
+#  print shares 
   clan_total_battles = clan_total_battles +  battle_count
 
 print "Total,Clan," + str(clan_total_battles)
+
+#if None != gold:
+#  gold_issue = 0
+#  game_share = gold / clan_total_battles
+##  print "game share is " + str(gold) + "/" + str(clan_total_battles) + " = " + str(game_share)
+#  for id in shares:
+#    p = shares[str(id)]
+#    b = p['battles']
+#    player_share = b * game_share
+#    #player_share = math.floor(player_share)
+#    #player_share = player_share
+#    print p['account_name'] + "player_share: " + str(b) + " * " + str(game_share) + " = " + str(player_share)
+#    p['share'] = player_share
+#    gold_issue = gold_issue + player_share
+#    print p
+#  
+#print "Gold allocated for issue: " + str (gold)
+#print "Gold issued: " + str(gold_issue)
+#print "Unissued due to rounding: " + str( gold - gold_issue) 
+
+
